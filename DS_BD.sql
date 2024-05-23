683bdbf31fce14bc2aa9577ef4b5db9391aac4c5
Davi Kirk <126596719+DaviKirk@users.noreply.github.com>
2024-05-23 14:41:05 -0300

    Add files via upload

---
+++ DS_BD.sql
@@ -0,0 +1,8 @@
+create database crudcharp
+create table colaborador(
+codigo_colaborador int not null,
+nome_colaborador varchar(50),
+sobrenome_colaborador varchar(50),
+cpf_colaborador varchar(50),
+primary key(codigo_colaborador)
+);
